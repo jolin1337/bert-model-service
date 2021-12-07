4fe21cc3bf32ebcf6e7cc7d58bfecfd71d2b2238
Johannes Lindén <johannes.linden@bolagsverket.se>
2021-12-07 13:54:25 +0100

    Fix user permissions

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
+import named_entity_recognition.api as ner_api
+import part_of_speech.api as pos_api
+import tokenizer.api as tok_api
 from app import app
-from named_entity_recognition import api as ner_api
-from named_entity_recognition import get_model as get_ner_model
-from part_of_speech import api as pos_api
-from part_of_speech import get_model as get_pos_model
-from tokenizer import api as tok_api
-from tokenizer import get_model as get_tok_model
+from named_entity_recognition.model import get_model as get_ner_model
+from part_of_speech.model import get_model as get_pos_model
+from tokenizer.model import get_model as get_tok_model
 
 get_ner_model()
 get_pos_model()
