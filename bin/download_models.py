e195cbf098f237bb6f145db6956f63c825c193bb
Johannes Lindén <johannes.linden@bolagsverket.se>
2021-12-07 11:24:12 +0100

    Update download script

---
+++ bin/download_models.py
@@ -0,0 +1,7 @@
+from named_entity_recognition.model import get_model as get_ner_model
+from part_of_speech.model import get_model as get_pos_model
+from tokenizer.model import get_model as get_tok_model
+
+get_ner_model()
+get_pos_model()
+get_tok_model()
